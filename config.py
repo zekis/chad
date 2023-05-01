9b44b195faf414fc175afa4eb6bfea28266d9f0d
Zeke Tierney <zeke.tierney@sgcontrols.com.au>
2023-05-01 10:55:38 +0800

    First Version

---
+++ config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+# Access the environment variables in your code
+PORT = 3978
+APP_ID = os.getenv("MicrosoftAppId")
+APP_PASSWORD = os.getenv("MicrosoftAppPassword")
