b592c96c81e336e5f25be58cd2b7b22d93c78ed4
Zeke Tierney <zeke.tierney@sgcontrols.com.au>
2023-05-03 21:58:16 +0800

    trying autoGTP

--- bots/teams_conversation_bot.py
+++ bots/teams_conversation_bot.py
@@ -2,10 +2,10 @@
 # Licensed under the MIT License.
 from dotenv import find_dotenv, load_dotenv
 import os
+from pathlib import Path
 import json
 import random
 import requests
-
 
 from typing import List
 from botbuilder.core import CardFactory, TurnContext, MessageFactory
@@ -17,10 +17,9 @@ from botbuilder.schema._connector_client_enums import ActionTypes
 
 from typing import Dict
 
-
 from botbuilder.schema import ChannelAccount, ConversationReference, Activity
-
-
+from langchain.experimental import AutoGPT
+from langchain.chat_models import ChatOpenAI
 from langchain.agents import load_tools
 from langchain.agents import ZeroShotAgent, AgentExecutor
 from langchain.memory import ConversationBufferMemory
@@ -28,6 +27,26 @@ from langchain import OpenAI, LLMChain
 from langchain.llms import OpenAI
 from langchain.agents import tool
 
+from langchain.tools.file_management import (
+    ReadFileTool,
+    CopyFileTool,
+    DeleteFileTool,
+    MoveFileTool,
+    WriteFileTool,
+    ListDirectoryTool,
+)
+from langchain.agents.agent_toolkits import FileManagementToolkit
+from tempfile import TemporaryDirectory
+
+
+from langchain.vectorstores import FAISS
+from langchain.docstore import InMemoryDocstore
+from langchain.embeddings import OpenAIEmbeddings
+
+# Define your embedding model
+embeddings_model = OpenAIEmbeddings()
+# Initialize the vectorstore as empty
+import faiss
 
 thinking_messages = [
     "Just a moment...",
@@ -56,18 +75,23 @@ ADAPTIVECARDTEMPLATE = "resources/UserMentionCardTemplate.json"
 
 class TeamsConversationBot(TeamsActivityHandler):
     
-    
-
     load_dotenv(find_dotenv())
+    # We'll make a temporary directory to avoid clutter
+    
     llm = OpenAI(temperature=0)
-
-    tools = load_tools(["human","wikipedia", "google-search", "llm-math", "requests"], llm=llm)
+    toolkit = FileManagementToolkit()
+    #toolkit.get_tools()
+    tools = load_tools(["wikipedia", "google-search", "llm-math", "requests_all"], llm=llm)
     # Assuming the "human" tool is the first one in the list
     # Set the custom prompt and input functions
-    
+    tools.extend(toolkit.get_tools())
+
+    embedding_size = 1536
+    index = faiss.IndexFlatL2(embedding_size)
+    vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
 
     prefix = """Have a conversation with a human, answering the following questions as best you can. You have access to the following tools:"""
-    suffix = """and the windows terminal. Begin!"
+    suffix = """Begin!"
 
     {chat_history}
     Question: {input}
@@ -79,42 +103,28 @@ class TeamsConversationBot(TeamsActivityHandler):
         suffix=suffix, 
         input_variables=["input", "chat_history", "agent_scratchpad"]
     )
-    memory = ConversationBufferMemory(memory_key="chat_history")
+    #memory = ConversationBufferMemory(memory_key="chat_history")
 
-    llm_chain = LLMChain(llm=OpenAI(temperature=0), prompt=prompt)
-    agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
-    agent_chain = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory) 
-    
+    #llm_chain = LLMChain(llm=OpenAI(temperature=0), prompt=prompt)
+    #agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
+    #agent_chain = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory) 
+    agent = AutoGPT.from_llm_and_tools(
+        ai_name="Tom",
+        ai_role="Assistant",
+        tools=tools,
+        llm=ChatOpenAI(temperature=0),
+        memory=vectorstore.as_retriever()
+    )
+    # Set verbose to be true
+    agent.chain.verbose = True
 
 
-    def __init__(self, app_id: str, app_password: str, conversation_references: Dict[str, ConversationReference]):
+    def __init__(self, app_id: str, app_password: str):
         self._app_id = app_id
         self._app_password = app_password
-        self.conversation_references = conversation_references
-
-        self.tools[0].prompt_func = self.custom_prompt_func
-        self.tools[0].input_func = self.custom_input_func
-
-       
-
-    async def on_conversation_update_activity(self, turn_context: TurnContext):
-        self._add_conversation_reference(turn_context.activity)
-        return await super().on_conversation_update_activity(turn_context)
-
-    async def on_teams_members_added(  # pylint: disable=unused-argument
-        self,
-        teams_members_added: TeamsChannelAccount,
-        team_info: TeamInfo,
-        turn_context: TurnContext,
-    ):
-        for member in teams_members_added:
-            if member.id != turn_context.activity.recipient.id:
-                await turn_context.send_activity(
-                    f"Welcome to the team { member.given_name } { member.surname }. "
-                )
+        
 
     async def on_message_activity(self, turn_context: TurnContext):
-        self._add_conversation_reference(turn_context.activity)
         TurnContext.remove_recipient_mention(turn_context.activity)
         text = turn_context.activity.text.strip().lower()
 
@@ -126,184 +136,6 @@ class TeamsConversationBot(TeamsActivityHandler):
         #await self._send_card(response, False)
         return
 
-    async def _mention_adaptive_card_activity(self, turn_context: TurnContext):
-        TeamsChannelAccount: member = None
-        try:
-            member = await TeamsInfo.get_member(
-                turn_context, turn_context.activity.from_property.id
-            )
-        except Exception as e:
-            if "MemberNotFoundInConversation" in e.args[0]:
-                await turn_context.send_activity("Member not found.")
-                return
-            else:
-                raise
-
-        card_path = os.path.join(os.getcwd(), ADAPTIVECARDTEMPLATE)
-        with open(card_path, "rb") as in_file:
-            template_json = json.load(in_file)
-        
-        for t in template_json["body"]:
-            t["text"] = t["text"].replace("${userName}", member.name)        
-        for e in template_json["msteams"]["entities"]:
-            e["text"] = e["text"].replace("${userName}", member.name)
-            e["mentioned"]["id"] = e["mentioned"]["id"].replace("${userUPN}", member.user_principal_name)
-            e["mentioned"]["id"] = e["mentioned"]["id"].replace("${userAAD}", member.additional_properties["aadObjectId"])
-            e["mentioned"]["name"] = e["mentioned"]["name"].replace("${userName}", member.name)
-        
-        adaptive_card_attachment = Activity(
-            attachments=[CardFactory.adaptive_card(template_json)]
-        )
-        await turn_context.send_activity(adaptive_card_attachment)
-
-    async def _mention_activity(self, turn_context: TurnContext):
-        mention = Mention(
-            mentioned=turn_context.activity.from_property,
-            text=f"<at>{turn_context.activity.from_property.name}</at>",
-            type="mention",
-        )
-
-        reply_activity = MessageFactory.text(f"Hello {mention.text}")
-        reply_activity.entities = [Mention().deserialize(mention.serialize())]
-        await turn_context.send_activity(reply_activity)
-
-    async def _send_card(self, turn_context: TurnContext, isUpdate):
-        buttons = [
-            CardAction(
-                type=ActionTypes.message_back,
-                title="Message all members",
-                text="messageallmembers",
-            ),
-            CardAction(type=ActionTypes.message_back, title="Who am I?", text="whoami"),
-            CardAction(type=ActionTypes.message_back, title="Find me in Adaptive Card", text="mention me"),
-            CardAction(
-                type=ActionTypes.message_back, title="Delete card", text="deletecard"
-            ),
-        ]
-        if isUpdate:
-            await self._send_update_card(turn_context, buttons)
-        else:
-            await self._send_welcome_card(turn_context, buttons)
-
-    async def _send_welcome_card(self, turn_context: TurnContext, buttons):
-        buttons.append(
-            CardAction(
-                type=ActionTypes.message_back,
-                title="Update Card",
-                text="updatecardaction",
-                value={"count": 0},
-            )
-        )
-        card = HeroCard(
-            title="Welcome Card", text="Click the buttons.", buttons=buttons
-        )
-        await turn_context.send_activity(
-            MessageFactory.attachment(CardFactory.hero_card(card))
-        )
-
-    async def _send_update_card(self, turn_context: TurnContext, buttons):
-        data = turn_context.activity.value
-        data["count"] += 1
-        buttons.append(
-            CardAction(
-                type=ActionTypes.message_back,
-                title="Update Card",
-                text="updatecardaction",
-                value=data,
-            )
-        )
-        card = HeroCard(
-            title="Updated card", text=f"Update count {data['count']}", buttons=buttons
-        )
-
-        updated_activity = MessageFactory.attachment(CardFactory.hero_card(card))
-        updated_activity.id = turn_context.activity.reply_to_id
-        await turn_context.update_activity(updated_activity)
-
-    async def _get_member(self, turn_context: TurnContext):
-        TeamsChannelAccount: member = None
-        try:
-            member = await TeamsInfo.get_member(
-                turn_context, turn_context.activity.from_property.id
-            )
-        except Exception as e:
-            if "MemberNotFoundInConversation" in e.args[0]:
-                await turn_context.send_activity("Member not found.")
-            else:
-                raise
-        else:
-            await turn_context.send_activity(f"You are: {member.name}")
-
-    async def _message_all_members(self, turn_context: TurnContext):
-        team_members = await self._get_paged_members(turn_context)
-
-        for member in team_members:
-            conversation_reference = TurnContext.get_conversation_reference(
-                turn_context.activity
-            )
-
-            conversation_parameters = ConversationParameters(
-                is_group=False,
-                bot=turn_context.activity.recipient,
-                members=[member],
-                tenant_id=turn_context.activity.conversation.tenant_id,
-            )
-
-            async def get_ref(tc1):
-                conversation_reference_inner = TurnContext.get_conversation_reference(
-                    tc1.activity
-                )
-                return await tc1.adapter.continue_conversation(
-                    conversation_reference_inner, send_message, self._app_id
-                )
-
-            async def send_message(tc2: TurnContext):
-                return await tc2.send_activity(
-                    f"Hello {member.name}. I'm a Teams conversation bot."
-                )  # pylint: disable=cell-var-from-loop
-
-            await turn_context.adapter.create_conversation(
-                conversation_reference, get_ref, conversation_parameters
-            )
-
-        await turn_context.send_activity(
-            MessageFactory.text("All messages have been sent")
-        )
-
-    async def _get_paged_members(
-        self, turn_context: TurnContext
-    ) -> List[TeamsChannelAccount]:
-        paged_members = []
-        continuation_token = None
-
-        while True:
-            current_page = await TeamsInfo.get_paged_members(
-                turn_context, continuation_token, 100
-            )
-            continuation_token = current_page.continuation_token
-            paged_members.extend(current_page.members)
-
-            if continuation_token is None:
-                break
-
-        return paged_members
-
-    async def _delete_card_activity(self, turn_context: TurnContext):
-        await turn_context.delete_activity(turn_context.activity.reply_to_id)
-
-
-    def _add_conversation_reference(self, activity: Activity):
-        """
-        This populates the shared Dictionary that holds conversation references. In this sample,
-        this dictionary is used to send a message to members when /api/notify is hit.
-        :param activity:
-        :return:
-        """
-        conversation_reference = TurnContext.get_conversation_reference(activity)
-        self.conversation_references[
-            conversation_reference.user.id
-        ] = conversation_reference
-
 
     #this bot needs to provide similar commands as autoGPT except the commands are based on Check Email, Check Tasks, Load Doc, Load Code etc.
     def model_response(self, msg):
@@ -312,7 +144,7 @@ class TeamsConversationBot(TeamsActivityHandler):
             if msg == 'agent.template':
                 response = "NA:"
                 return response
-            response = self.agent_chain.run(msg)
+            response = self.agent.run([msg])
             #history.chat_memory.add_ai_message(response)
             return response
         except Exception as e:
@@ -324,11 +156,3 @@ class TeamsConversationBot(TeamsActivityHandler):
         response = requests.post(url, data=json.dumps(data), headers=headers, timeout=5.0)
         return response
     
-
-    def custom_prompt_func(prompt: str, atr ) -> None:
-        print(prompt)
-        print(atr)
-        return atr
-        
-    def custom_input_func(atr) -> str:
-        return atr
